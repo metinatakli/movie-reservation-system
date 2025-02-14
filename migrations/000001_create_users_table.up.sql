c090be89f21e9e614e09becc302fe4121117b552
Metin Atakli <m.berkay99@gmail.com>
2025-02-14 23:15:55 +0300

    Add migration files for users table

---
+++ migrations/000001_create_users_table.up.sql
@@ -0,0 +1,18 @@
+CREATE EXTENSION IF NOT EXISTS "citext";    -- For case-insensitive email addresses
+
+CREATE TYPE gender_type AS ENUM ('M', 'F', 'OTHER');
+
+CREATE TABLE IF NOT EXISTS users (
+    id bigserial PRIMARY KEY,
+    first_name text NOT NULL,
+    last_name text NOT NULL,
+    email citext UNIQUE NOT NULL,
+    password_hash bytea NOT NULL,
+    birth_date date NOT NULL,
+    gender gender_type NOT NULL,
+    created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+    updated_at timestamp(0) with time zone,
+    activated boolean NOT NULL DEFAULT FALSE,
+    is_active boolean NOT NULL DEFAULT TRUE,
+    version integer NOT NULL DEFAULT 1
+);
